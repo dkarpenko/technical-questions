14ef550b3da7b7b020cf7c5a016f651c747bc504
Denis Karpenko <denis@karpenko.me>
2015-09-22 23:28:01 +0300

    First solution

--- codeforces/round321div2/src/main/scala/me/karpenko/codeforces/round321div2/LongestSequence.scala
+++ codeforces/round321div2/src/main/scala/me/karpenko/codeforces/round321div2/LongestSequence.scala
@@ -2,24 +2,23 @@ package me.karpenko.codeforces.round321div2
 
 
 object LongestSequence {
+  def getTheLongestSequence(lengthOfCurrentSeq: Int, lengthOfTheLongestSeq: Int) =
+    if (lengthOfCurrentSeq > lengthOfTheLongestSeq) lengthOfCurrentSeq else lengthOfTheLongestSeq
+
+
+  def findLongestSequence(passedLength: Int, lengthOfTheLongestSeq: Int, currentValue: Int, values: Seq[Int]): Int = {
+    val lengthOfCurrentSeq = passedLength + 1
+    val updatedLongestLength = if (lengthOfCurrentSeq > lengthOfTheLongestSeq) lengthOfCurrentSeq else lengthOfTheLongestSeq
+
+    values.headOption.fold(getTheLongestSequence(lengthOfCurrentSeq, lengthOfTheLongestSeq)){ nextValue =>
+      val updatedCurrentSeq = if (nextValue >= currentValue) lengthOfCurrentSeq else 0
+      findLongestSequence(updatedCurrentSeq, updatedLongestLength, nextValue, values.tail)
+    }
+  }
+
 
   def findTheLongestSequence(values: Seq[Int]) = {
     values.headOption.fold(0) { findLongestSequence(0, 0, _, values.drop(1)) }
-
-    def getTheLongestSequence(lengthOfCurrentSeq: Int, lengthOfTheLongestSeq: Int) =
-      if (lengthOfCurrentSeq > lengthOfTheLongestSeq) lengthOfCurrentSeq else lengthOfTheLongestSeq
-
-
-    def findLongestSequence(passedLength: Int, lengthOfTheLongestSeq: Int, currentValue: Int, values: Seq[Int]): Int = {
-      val lengthOfCurrentSeq = passedLength + 1
-      val updatedLongestLength = if (lengthOfCurrentSeq > lengthOfTheLongestSeq) lengthOfCurrentSeq else lengthOfTheLongestSeq
-
-      values.headOption.fold(getTheLongestSequence(lengthOfCurrentSeq, lengthOfTheLongestSeq)){ nextValue =>
-        val updatedCurrentSeq = if (nextValue >= currentValue) lengthOfCurrentSeq else 0
-        findLongestSequence(updatedCurrentSeq, updatedLongestLength, nextValue, values.tail)
-      }
-    }
-
   }
 
   def main(args: Array[String]) {
@@ -29,6 +28,13 @@ object LongestSequence {
     val moneyPerDay = in.next().toString.split(" ").map(_.toInt)
 
     val growingPeriod = findTheLongestSequence(moneyPerDay)
+
+
+    val numbers = List(5, 4, 8, 6, 2)
+    println(numbers.fold(0) { (z, i) =>
+      z + i
+    })
+
 
     println(growingPeriod)
   }
